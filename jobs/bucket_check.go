2c4b148db31f07a7a92b5e92df31c9485e6c1b9a
alvin-reyes <me@areyes.onl>
2023-06-01 17:18:14 -0400

    reconfigure max size split

--- jobs/bucket_check.go
+++ jobs/bucket_check.go
@@ -62,8 +62,9 @@ func (r *BucketChecker) Run() error {
 	bucket.Status = dealResult.Content.Status
 	if dealResult.Deals != nil && len(dealResult.Deals) > 0 {
 		bucket.DealId = int64(dealResult.Deals[0].DealID)
+		bucket.DealUuid = dealResult.Deals[0].DealUUID
 	}
-	bucket.DealUuid = dealResult.Deals[0].DealUUID
+
 	r.LightNode.DB.Save(&bucket)
 
 	// if the updated date is 1 day old, then we should just retry the request
